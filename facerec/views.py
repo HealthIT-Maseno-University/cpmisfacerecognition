a2d33085abcb82762c188c4380f68d845c7754d7
stephen wanjala <stephenwanjala145@gmail.com>
2023-05-07 11:19:23 +0300

    define Structural Similarity Index, Mean Squared Error, Normalized Cross-Correlation and Histogram comparison functions

--- facerec/views.py
+++ facerec/views.py
@@ -1,10 +1,12 @@
 # Create your views here.
 import cv2
 import face_recognition
+import numpy as np
 from django.conf import settings
 from django.contrib import messages
 from django.core.files.storage import default_storage
 from django.shortcuts import render, redirect
+from tensorflow.python.ops.image_ops_impl import ssim
 
 from .models import MissingChild
 
@@ -56,4 +58,55 @@ def checkResemblance(images,filename):
             matches.append(child)
     # render results template
     return matches
+
+
+def compute_ssim_score(img1, img2):
+    try:
+        win_size = min(img1.shape[:2])
+        win_size = (win_size // 2) * 2 + 1  # Ensure odd window size
+        sigma = 1.5 * win_size / 11
+        if img1.ndim > 2:
+            channel_axis = 2
+        else:
+            channel_axis = None
+        ssim_score = ssim(img1, img2, win_size=win_size, sigma=sigma, multichannel=True, channel_axis=channel_axis)
+        return ssim_score
+    except:
+        return 0
+
+
+def compute_mse_score(img1, img2):
+    try:
+        if img1.shape == img2.shape:
+            mse_score = np.mean((img1 - img2) ** 2)
+        else:
+            mse_score = None
+        return mse_score
+    except:
+        return 0
+
+
+def compute_ncc_score(img1, img2):
+    try:
+        result = cv2.matchTemplate(img1, img2, cv2.TM_CCORR_NORMED)
+        ncc_score = cv2.minMaxLoc(result)[1]
+        return ncc_score
+    except:
+        return 0
+
+
+def compute_histogram_score(img1, img2):
+    try:
+        hist1 = cv2.calcHist([img1], [0, 1, 2], None, [8, 8, 8], [0, 256, 0, 256, 0, 256])
+        hist2 = cv2.calcHist([img2], [0, 1, 2], None, [8, 8, 8], [0, 256, 0, 256, 0, 256])
+        histogram_score = cv2.compareHist(hist1, hist2, cv2.HISTCMP_CORREL)
+        return histogram_score
+    except:
+        return 0
+
+
+
+
+
+
 
